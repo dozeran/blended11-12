0419c00fc029bfaa89351a319243b36c27d47baa
DenisVdovichenko <128327375+DenisVdovichenko@users.noreply.github.com>
2023-07-02 16:57:48 +0300

    pora_roky

--- js/module-1.js
+++ js/module-1.js
@@ -30,3 +30,34 @@
 
 // цикли for, while ===================
 // конструктор switch ==================
+
+// Змінна num може набувати 4 значення: 1, 2, 3 або 4. Якщо вона має
+// значення '1', то у змінну result запишемо 'зима', якщо має значення
+// '2' - 'весна' і так далі. Розв'яжіть завдання через switch-case.
+
+const num = 0;
+let result = "";
+
+switch (num) {
+  case 1:
+    result = "зима";
+
+    break;
+  case 2:
+    result = "весна";
+
+    break;
+  case 3:
+    result = "осінь";
+
+    break;
+  case 4:
+    result = "літо";
+
+    break;
+
+  default:
+    result = "Не являється порою року";
+    break;
+}
+console.log(result);
