84ad3e5d300e42efb4b138b59f4bedb6b078f418
dozeran <dozeran@proton.me>
2023-07-30 16:12:03 +0300

    update next

--- lesson-3/module_4-5.js
+++ lesson-3/module_4-5.js
@@ -21,3 +21,30 @@
 
 // // console.log(letMeSeeYourName(greet));
 // letMeSeeYourName(greet);
+
+//TODO № 2 ==============================================
+//Напишіть дві функції
+//makeProduct(name, price, callback) - приймає
+//ім'я та ціну товару, а також callback.
+//Функція створює об'єкт товару, додаючи йому унікальний
+//ідентифікатор як id і викликає callback
+//Передаючи йому створений об'єкт.
+//showProduct(product) - коллбек приймаючий об'єкт
+//продукту і логірующий їх у консоль
+
+// function makeProduct(name, price, callback) {
+//   const product = {
+//     name,
+//     price,
+//     id: Math.random(),
+//   };
+
+//   return callback(product);
+// }
+
+// function showProduct(product) {
+//   console.log(product);
+// }
+
+// makeProduct("tablet", 6000, showProduct);
+// makeProduct("iphone", 21000, showProduct);
