599eaa5ac3d00a3e4cd375acf4ee4eca06250f05
ArtemCherchuk <250artem250@gmail.com>
2023-08-13 15:03:00 +0200

    commit

---
+++ lesson-4/module-4.js
@@ -0,0 +1,16 @@
+//TODO: № 1 ==============================================
+// Натискання на кнопку "SHOW ME" має виводити значення з поля введення (дивіться на елементи в html-розмітці)
+// console.log("Hello");
+
+// const btnShowME = document.querySelector("#alertButton");
+// const inputEl = document.querySelector("#alertInput");
+
+// const onTextInput = () => {
+//   const inputVal = btnShowME.nextElementSibling.value;
+//   console.log("🚀 ~ onTextInput ~ inputVal:", inputVal);
+// const inputEl = document.querySelector("#alertInput").value;
+// console.log("🚀 ~ onTextInput ~ inputEl:", inputEl);
+// alert(inputEl.value);
+// };
+
+// btnShowME.addEventListener("click", onTextInput);
